53015019a4d6cbd617c50c42e4d2bc65983fef58
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-05-06 18:54:49 -0400

    created the Linked List interface

---
+++ ScratchImplemetations/src/DS/LinkedList.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package DS;
+
+/**
+ * @author Saad
+ *
+ */
+public interface LinkedList<E> {
+	
+	public int length();
+	
+	public E get();
+	
+	public void set(int index, E element);
+	
+	public void addFirst(E element);
+	
+	public void addLast(E element);
+	
+	public void addBetween(int index, E element);
+	
+	public E remove(int index);
+	
+	public int indexOf(E element);
+}
