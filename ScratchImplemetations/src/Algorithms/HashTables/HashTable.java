761b72fa746e9fe38a351689d7c7e66f36ba6f3a
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-05-09 18:35:06 -0400

    roughly implemented the insertPair method. Testing to follow

--- ScratchImplemetations/src/Algorithms/HashTables/HashTable.java
+++ ScratchImplemetations/src/Algorithms/HashTables/HashTable.java
@@ -23,7 +23,22 @@ public class HashTable<K, V> {
 			array[keyHash].add(pair);
 			isAdded=true;
 		}
-		
+		else {
+			int counter=-1;
+			for(int i=0;i<array[keyHash].size();i++) {
+				if(array[keyHash].get(i).equals(pair)) {
+					counter=i;
+					break;
+				}
+			}
+			if(counter==-1) {
+				array[keyHash].addLast(pair);
+				isAdded=true;
+			}else {
+				array[keyHash].get(counter).setValue(pair.getValue());			
+			}
+		}
+		return isAdded;
 	}
 	
 }
