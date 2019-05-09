32e6759d20b01eb2affcc8abdeec5eaf8f846867
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-05-09 16:29:18 -0400

    finished implementing the pair class that will hold k,v pairs

---
+++ ScratchImplemetations/src/Algorithms/HashTables/Pair.java
@@ -0,0 +1,25 @@
+package Algorithms.HashTables;
+
+public class Pair<K, V> {
+	
+	private K key;
+	private V value;
+	
+	public Pair(K key, V value) {
+		this.setKey(key);
+		this.setValue(value);
+	}
+	
+	public K getKey() {
+		return key;
+	}
+	public void setKey(K key) {
+		this.key = key;
+	}
+	public V getValue() {
+		return value;
+	}
+	public void setValue(V value) {
+		this.value = value;
+	}
+}
