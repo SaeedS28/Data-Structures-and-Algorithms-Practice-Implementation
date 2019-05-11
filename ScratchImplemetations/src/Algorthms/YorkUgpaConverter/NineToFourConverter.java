99edde0c16ffb0ae06a5b080d5541b27bf1e9834
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-05-11 13:48:58 -0400

    got the basic skeleton of a GPA converter program going

---
+++ ScratchImplemetations/src/Algorthms/YorkUgpaConverter/NineToFourConverter.java
@@ -0,0 +1,26 @@
+package Algorthms.YorkUgpaConverter;
+import java.util.Scanner;
+
+// Conversion Table can be found at:
+// https://gradecalc.info/ca/on/yorku/gpa_calc.pl
+// and https://laps.yorku.ca/student-resources/gpa-calculator/
+
+public class NineToFourConverter {
+
+	public static void main(String[] args) {
+		System.out.println("This program takes your YorkU GPA and converts it to the traditional 4.0 scale\n\n");
+		System.out.print("Please enter the number of courses taken so far (IGNORE PASS/FAIL COURSES): ");
+		
+		Scanner input=new Scanner(System.in);
+		
+		int counter=input.nextInt();
+		
+
+	}
+
+	public static int gradeToNumericalConverter(String grade) {
+		
+		return 0;
+	}
+	
+}
