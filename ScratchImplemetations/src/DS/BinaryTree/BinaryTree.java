df1a9e49725313a021b8b0b03f6cf3156169077b
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-05-11 00:41:32 -0400

    template created for the BinaryTree class

---
+++ ScratchImplemetations/src/DS/BinaryTree/BinaryTree.java
@@ -0,0 +1,44 @@
+package DS.BinaryTree;
+
+public class BinaryTree<V> implements BinaryTreeInterface<V> {
+	private int numberOfElements;
+	private Entry<V> root;
+	
+	public void addEntry(Entry<V> entry) {
+		
+	}
+
+	public V findValue(int key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void postOrderTraversal() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void preOrderTraversal() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void inOrderTraversal() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Entry<V> removeEntry(int key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getNumberOfElements() {
+		return numberOfElements;
+	}
+
+	public void setNumberOfElements(int numberOfElements) {
+		this.numberOfElements = numberOfElements;
+	}
+
+}
