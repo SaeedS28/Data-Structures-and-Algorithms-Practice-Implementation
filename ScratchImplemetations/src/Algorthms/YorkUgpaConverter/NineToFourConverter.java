07736ff6be15febd51b69db78257e1b2141508ff
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-05-11 17:48:11 -0400

    created the method that maps the grades to a 4-scale

--- ScratchImplemetations/src/Algorthms/YorkUgpaConverter/NineToFourConverter.java
+++ ScratchImplemetations/src/Algorthms/YorkUgpaConverter/NineToFourConverter.java
@@ -66,4 +66,31 @@ public class NineToFourConverter {
 		}
 	}
 	
+	public static double gradeToNumericalConverterFour(String grade) {
+		
+		switch(grade) {
+		case "A+":
+			return 4;
+		case "A":
+			return 3.8;
+		case "B+":
+			return 3.3;
+		case "B":
+			return 3;
+		case "C+":
+			return 2.3;
+		case "C":
+			return 2;
+		case "D+":
+			return 1.3;
+		case "D":
+			return 1;
+		case "E":
+			return 0.7;
+		case "F":
+			return 0;
+		default:
+			return 0;
+	}
+	}
 }
