ee49995981881bebf10cceb507d153b292319186
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-05-19 12:47:33 -0400

    Stack interface completed

---
+++ ScratchImplemetations/src/DS/Stack and Queue/Stack.java
@@ -0,0 +1,15 @@
+/**
+ * Stack
+ */
+public interface Stack<E> {
+    public boolean isEmpty();
+
+    public E peek();
+
+    public E pop();
+
+    public void push();
+
+    public int search();
+
+}
