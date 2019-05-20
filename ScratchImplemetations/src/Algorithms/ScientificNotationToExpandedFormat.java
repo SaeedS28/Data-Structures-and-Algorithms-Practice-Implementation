a51791394bdc6706a32d4cd2ea6def05c07fc515
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-05-20 17:22:09 -0400

    Scientific notation to numeral converter done

--- ScratchImplemetations/src/Algorithms/ScientificNotationToExpandedFormat.java
+++ ScratchImplemetations/src/Algorithms/ScientificNotationToExpandedFormat.java
@@ -7,7 +7,7 @@ public class ScientificNotationToExpandedFormat {
 	public static void main(String[] args) {
 		final String regexForProperSN = "-?\\d+(\\.?\\d*)[xX]{1}10\\^-?\\d+$";
 		
-		System.out.print("Enter a number in scientific notation to convert it to its expanded form.\t"
+		System.out.print("Enter a number in scientific notation to convert it to its expanded form.\n"
 				+ "For example -3x10^-13 or 3.26X10^12\n\nEnter it now: ");
 		
 		Scanner input= new Scanner(System.in);
@@ -18,9 +18,24 @@ public class ScientificNotationToExpandedFormat {
 		boolean matches = string.matches(regexForProperSN);
 		
 		if(matches) {
-			System.out.println("Yay");
+			double numeralPart=Double.parseDouble(string.substring(0, string.toLowerCase().indexOf("x")));
+			//System.out.println(numeralPart);
+			
+			int power = Integer.parseInt(string.substring(string.indexOf("^")+1));
+			//System.out.println(power);
+			
+			if(power<0) {
+				for(int i=0;i<Math.abs(power);i++) {
+					numeralPart=numeralPart/10;
+				}
+			}else {
+				for(int i=0;i<power;i++) {
+					numeralPart=numeralPart*10;
+				}
+			}
+			System.out.println("\nThe number in its numeral format is: " + numeralPart);
 		}else {
-			System.out.println("Nay");
+			System.out.println("The expression you have entered is syntactiacally incorrect. Try again.");
 		}
 		
 		
