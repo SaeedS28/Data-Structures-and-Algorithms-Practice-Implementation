b392ff92837eddbac4f4a7682c808df41b388653
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-05-22 00:01:14 -0400

    Don't know what I did, but O(1) complexity attained

--- ScratchImplemetations/src/Algorithms/ArrayProductExceptSelf.java
+++ ScratchImplemetations/src/Algorithms/ArrayProductExceptSelf.java
@@ -13,23 +13,23 @@ public class ArrayProductExceptSelf {
 	}
 	
 	public static int[] product(int[] array){
-		int[] leftProd = new int[array.length];
-		int[] rightProd = new int[array.length];
 		int[] prod = new int[array.length];
+		int placeHolder=1;
 		
-		leftProd[0]=1;
-		rightProd[array.length-1]=1;
-		
-		for(int i=1;i<array.length;i++) {
-			leftProd[i]=leftProd[i-1]*array[i-1];
+		for(int i=0;i<array.length;i++) {
+			prod[i]=1;
 		}
 		
-		for(int i = array.length-2; i>=0; i--) {
-			rightProd[i]=rightProd[i+1]*array[i+1];
+		for(int i=0; i<array.length;i++) {
+			prod[i]=placeHolder;
+			placeHolder = placeHolder*array[i];
 		}
 		
-		for(int i = 0;i<array.length;i++) {
-			prod[i]=leftProd[i]*rightProd[i];
+		placeHolder=1;
+		
+		for(int i=array.length-1;i>=0;i--) {
+			prod[i]=prod[i]*placeHolder;
+			placeHolder = placeHolder*array[i];
 		}
 		
 		return prod;
