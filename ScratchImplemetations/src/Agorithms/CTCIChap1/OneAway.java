220991c946e93306476e425c5905f786239f54dc
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-05-25 15:01:19 -0400

    case 3: same length, different edits implemented

--- ScratchImplemetations/src/Agorithms/CTCIChap1/OneAway.java
+++ ScratchImplemetations/src/Agorithms/CTCIChap1/OneAway.java
@@ -6,17 +6,27 @@ package Agorithms.CTCIChap1;
 public class OneAway {
     public static void main(String[] args) {
         System.out.println(isOneOrZeroEditsAway("Saad", "Saad"));   // returns true because strings are same
+        System.out.println(isOneOrZeroEditsAway("Sasd", "Snad"));   // returns false because 2 edits, even though same length
         System.out.println(isOneOrZeroEditsAway("Saadss", "Saads"));   // returns false because diff of abs of their lengths is >1
     }
 
     public static boolean isOneOrZeroEditsAway(String a, String b) {
         String shorter, longer;
+        int differences=0;
+        int j=0;
 
-        if(a.equals(b)){
+        if(a.equals(b)){    //equal
             return true;
-        } else if(Math.abs(a.length()-b.length())>1){
-            System.out.println("off by > 1");
-            return false;
+        } else if(a.length()==b.length()){  //same length
+            System.out.println("Same length");
+            for (int i = 0; i < a.length(); i++) {
+                if (a.charAt(i)!=b.charAt(i)) {
+                    ++differences;
+                    System.out.println("Differences incremented to: "+differences);
+                    if(differences>1) return false;
+                }
+            }
+            return true;
         }
 
         return false;
