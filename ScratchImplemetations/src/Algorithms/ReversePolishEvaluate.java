1aca047a89983069a1fd90d923813757c1d23dc6
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-06-01 00:09:36 -0400

    completed the reverse polish notation evaluator

--- ScratchImplemetations/src/Algorithms/ReversePolishEvaluate.java
+++ ScratchImplemetations/src/Algorithms/ReversePolishEvaluate.java
@@ -10,13 +10,15 @@ public class ReversePolishEvaluate {
     public static void main(String[] args) {
         // First expression
         LinkedList<String> exp1= new LinkedList<>();
+        // ["4", "13", "5", "/", "+"]
         exp1.add("4");
         exp1.add("13");
         exp1.add("5");
         exp1.add("/");
         exp1.add("+");
-        //evaluate(exp1);
+        evaluate(exp1);
 
+        // Second expression
         LinkedList<String> exp2= new LinkedList<>();
         // ["2", "1", "+", "3", "*"]
         exp2.add("2");
@@ -25,23 +27,36 @@ public class ReversePolishEvaluate {
         exp2.add("3");
         exp2.add("*");
         evaluate(exp2);
-        
+
+        // Third expression
+        LinkedList<String> exp3= new LinkedList<>();
+        // ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+        exp3.add("10");
+        exp3.add("6");
+        exp3.add("9");
+        exp3.add("3");
+        exp3.add("+");
+        exp3.add("-11");
+        exp3.add("*");
+        exp3.add("/");
+        exp3.add("*");
+        exp3.add("17");
+        exp3.add("+");
+        exp3.add("5");
+        exp3.add("+");
+        evaluate(exp3);
     }
 
     public static int evaluate(LinkedList<String> expression){
         
         int i=0;
-
+        System.out.println(expression.toString());
         while (i<expression.size()) {
             if (expression.get(i).equals("*") || expression.get(i).equals("+") || expression.get(i).equals("-") || expression.get(i).equals("/")) {
                 i=i-2;  // set back 2 places because this is where the sub-expression starts
-                System.out.println("value of i = "+i);
                 int num1 = Integer.parseInt(expression.remove(i));
-                System.out.println("Value of num1 = "+num1+"\tsize = "+expression.size());
                 int num2 = Integer.parseInt(expression.remove(i));
-                System.out.println("Value of num2 = "+num2+"\tsize = "+expression.size());
                 String operand = expression.remove(i);
-                System.out.println("Value of operand = "+operand+"\tsize = "+expression.size());
 
                 Integer answer;
                 if (operand.equals("*")) {
@@ -62,7 +77,7 @@ public class ReversePolishEvaluate {
             }
             i++;
         }
-        
-        return 0;
+        System.out.println("Expression evaluates to: "+expression.getFirst()+"\n");
+        return Integer.parseInt(expression.getFirst());
     }
 }
