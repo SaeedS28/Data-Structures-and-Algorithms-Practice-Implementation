14d3fa3ae5bfca6d3357799d651ca8dbca441cae
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-06-13 18:59:17 -0400

    finished implementing the dequeue method for the queue using stacks

--- ScratchImplemetations/src/DS/StackAndQueue/QueueUsingStack.java
+++ ScratchImplemetations/src/DS/StackAndQueue/QueueUsingStack.java
@@ -9,4 +9,33 @@ public class QueueUsingStack {
         
     }
     
+    private LinkedStack mainStack;
+    private LinkedStack auxStack;
+
+    public QueueUsingStack() {
+        mainStack = new LinkedStack();
+        auxStack = new LinkedStack();    
+    }
+
+    public void enqueue(Integer x) {
+        mainStack.push(x);
+    }
+
+    public Integer dequeue() {
+        Integer getFirst;
+        while (!mainStack.isEmpty()) {
+            auxStack.push(mainStack.pop());
+        }
+
+        getFirst = auxStack.pop();
+
+        while (!auxStack.isEmpty()) {
+            mainStack.push(auxStack.pop());
+        }
+        return getFirst;
+    }
+
+    public boolean isEmpty() {
+        return mainStack.isEmpty();
+    }
 }
