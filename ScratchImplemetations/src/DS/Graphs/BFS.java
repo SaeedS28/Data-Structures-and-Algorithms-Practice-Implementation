1719b9e0fa2e9919175b1f81e1caf7f969a23d66
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-06-18 18:36:09 -0400

    Finished Implementing BFS algorithm

--- ScratchImplemetations/src/DS/Graphs/BFS.java
+++ ScratchImplemetations/src/DS/Graphs/BFS.java
@@ -1,5 +1,6 @@
 package DS.Graphs;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
@@ -14,7 +15,7 @@ public class BFS<T> {
         Node<Integer> node3 = new Node<>(3);
         Node<Integer> node4 = new Node<>(4);
         Node<Integer> node5 = new Node<>(5);
-
+        Node<Integer> subGraph1 = new Node<>(25);
         // Creating the actual graph structure
         node2.addNeighbours(node5);
         node3.addNeighbours(node5);
@@ -25,6 +26,7 @@ public class BFS<T> {
 
         Graph<Integer> graph = new Graph<>();
         graph.addComponent(node1);
+        graph.addComponent(subGraph1);
 
         BFS<Integer> test = new BFS<>();
 
@@ -32,7 +34,29 @@ public class BFS<T> {
     }
 
     public void BreadthFirstSearch(Graph<T> graph) {
-           LinkedList<Node<T>> queue = new LinkedList<>(); // Will act as a Queue for BFS
+        LinkedList<Node<T>> queue = new LinkedList<>(); // Will act as a Queue for BFS
 
+        Iterator<Node<T>> subComponents = graph.getSubComporents();
+
+        while (subComponents.hasNext()) {
+            Node<T> component = subComponents.next();
+            component.setIsVisited(true);
+            queue.addLast(component);
+
+            while(!queue.isEmpty()){
+                Node<T> at = queue.remove();
+                System.out.println("Visited: "+at.getData());
+                
+                Iterator<Node<T>> neighbours = at.iterate();
+                
+                while (neighbours.hasNext()) {
+                    Node<T> nbr = neighbours.next();
+                    if (nbr.getIsVisited() == false) {
+                        nbr.setIsVisited(true);
+                        queue.addLast(nbr);
+                    }
+                }
+            }
+        }
     }
 }
