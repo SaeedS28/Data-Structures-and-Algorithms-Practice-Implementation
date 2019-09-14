fcf8694dd5309c80b1df065d38198ed7fb480af1
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-09-14 19:17:15 -0400

    logging order saving and retrieving works

--- PizzaOrder/src/com/fdmgroup/pizza/PizzaOrderController.java
+++ PizzaOrder/src/com/fdmgroup/pizza/PizzaOrderController.java
@@ -9,16 +9,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class PizzaOrderController {
-	Scanner scan = new Scanner(System.in);
-	int choice;
-	Pizza pizza = null;
-	int chk = 0;
-	int restore=-1;
+	private Scanner scan = new Scanner(System.in);
+	private int choice;
+	private Pizza pizza = null;
+	private int chk = 0;
+	private int restore=-1;
+	private static Logger orderLog = LogManager.getLogger("orderLog");
 	
 	void orderNewPizza() throws IOException {
 		System.out.println("Welcome to Saad's Pizzeria");
-
 		do {
 			System.out.println("\nPlease choose from the following toppings");
 			System.out.print("1. " + PizzaTopping.CHEESE + "\n2. " + PizzaTopping.CHICKEN + "\n3. "
@@ -91,35 +94,40 @@ public class PizzaOrderController {
 				
 				if(save == 'y') {
 					savePizzaOrder();
+				} else {
+					orderLog.info("Order completed but not saved.");
 				}
 			}
 		}else {
 			System.out.println("Nothing was ordered");
+			orderLog.info("No pizza ordered");
 		}
 	}
 
-	void createPizzaSavePoint() {
+	private void createPizzaSavePoint() {
 		try {
 			BufferedWriter bfw = new BufferedWriter(new FileWriter("incompleteOrders.txt",false));
 			System.out.println(pizza.getDescription());
 			bfw.close();
+			orderLog.info("Savepoint created.");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	void savePizzaOrder() {
+	private void savePizzaOrder() {
 		try {
 			BufferedWriter bfw = new BufferedWriter(new FileWriter("completedOrder.txt",false));
 			bfw.write(pizza.getDescription());
 			bfw.close();
 			System.out.println("Pizza Order saved successfully");
+			orderLog.info("Order saved as complete.");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	void orderFromSavePoint() {
+	private void orderFromSavePoint() {
 		File incomplete = new File("incompleteOrders.txt");
 		BufferedReader bfr = null;
 		String[] savedToppings = {};
@@ -141,6 +149,8 @@ public class PizzaOrderController {
 			for (int i = 2; i < savedToppings.length; i++) {
 				pizza = PizzaFactory.createPizza(savedToppings[i], pizza);
 			}
+			
+			orderLog.info("Order retrieved from last savepoint.");
 			chk = 1;
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -158,7 +168,7 @@ public class PizzaOrderController {
 		System.out.println("Last savepoint: " + pizza.getDescription() + "\nPrice: " + pizza.getPrice());
 	}
 
-	void reorderLast() {
+	private void reorderLast() {
 		File complete = new File("completedOrder.txt");
 		BufferedReader bfr = null;
 		String[] savedToppings = {};
@@ -181,6 +191,7 @@ public class PizzaOrderController {
 				pizza = PizzaFactory.createPizza(savedToppings[i], pizza);
 			}
 			restore=0;
+			orderLog.info("Ordered the last completed order.");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
